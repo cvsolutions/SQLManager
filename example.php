cd8bbc5004ace34cc6d40b4b25b2983d11bb7bb3
Concetto Infomedia <concetto@infomediaonline.it>
2012-11-27 15:44:08 +0100

    Example SQLManager

--- example.php
+++ example.php
@@ -10,19 +10,34 @@
 
  $SQL = new SQLManager();
 
- // $SQL->insert_mysql('nomi', array('value' => 'ciao mondo', 'tag' => '1,2,3,4,5'));
- // $SQL->update_mysql('nomi', array('value' => 'ciao mondo', 'tag' => '12345'), array('id' => 5));
- // $SQL->delete_mysql('nomi', array('id' => 5));
+ /** insert mysql database */
+ $SQL->insert_mysql('table', array(
+ 	'id' => '12345', 
+ 	'name' => 'Concetto'
+ 	));
 
- // $query = $SQL->query_mysql('SELECT * FROM nomi WHERE id = 6');
- // $result = $SQL->array_mysql($query);
+ /** update mysql database */
+ $SQL->update_mysql('table', array(
+ 	'name' => 'Concetto', 
+ 	'email' => 'user@localhost'
+ 	), array('id' => '12345'));
+
+ /** delete mysql database */
+ $SQL->delete_mysql('table', array(
+ 	'id' => '12345'
+ 	));
+
+ /** mysql query to single array */
+ $query = $SQL->query_mysql('SELECT * FROM table WHERE id = 6 LIMIT 0,1');
+ $result = $SQL->array_mysql($query);
  // print_r($result);
 
- // $data = array();
- // $result = $SQL->query_mysql('SELECT * FROM nomi');
- // while($row = $SQL->array_mysql($result)) {
- // $data[] = $row;
- // }
+ /** mysql query to loop array */
+ $data = array();
+ $result = $SQL->query_mysql('SELECT * FROM table ORDER BY id DESC');
+ while($row = $SQL->array_mysql($result)) {
+ 	$data[] = $row;
+ }
  // print_r($data);
 
  $SQL->close_mysql();
